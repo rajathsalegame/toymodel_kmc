38e5978aac99f2a29419fa83b44c85362e2dc115
Rajath Salegame <rajathsalegame@college.harvard.edu>
2020-06-29 01:18:15 -0500

    build graph class

---
+++ src/graph.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Graph:
+	def __init__(self, n_basins: int, microstates: list, weights: list):
+		pass
+
+class Basin:
+	def __init__(self, energy: float, microstates: list, weights: list):
+		pass 
+
+
+class Microstate:
+	def __init__(self, energy):
+		pass
+
+
+
