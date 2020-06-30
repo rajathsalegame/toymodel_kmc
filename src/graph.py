12da660aec3764d45069c0315a74dfd60518e456
Rajath Salegame <rajathsalegame@college.harvard.edu>
2020-06-30 03:20:29 -0500

    started graph.py module

--- src/graph.py
+++ src/graph.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 class Graph:
-	def __init__(self, n_basins: int, microstates: list, weights: list):
+	def __init__(self, basins: int, microstates: list, rates: list):
 		pass
 
 class Basin:
-	def __init__(self, energy: float, microstates: list, weights: list):
+	def __init__(self, energy: float, microstates: list, rates: list):
 		pass 
-
 
 class Microstate:
 	def __init__(self, energy):
